479b81bc1ece6e58994b74e93baac8a4f08c206c
Jim Munro <jimfmunro@gmail.com>
2011-12-20 08:49:32 -0700

    through chap 3

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,15 +1,29 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import Context
+from django.template.loader import get_template
+from django.contrib.auth.models import User
+
 def main_page(request):
-    output = u'''
-    <html>
-    <head><title>%s</title></head>
-    <body>
-    <h1>%s</h1><p>%s</p>
-    </body>
-    </html>
-    ''' % (
-    u'Django Bookmarks',
-    u'Welcome to Django Bookmarks',
-    u'Where you can store and share bookmarks!'
-    )
+    template = get_template('main_page.html')
+    variables = Context({
+        'head_title': u'Django Bookmarks',
+        'page_title': u'Welcome to Django Bookmarks',
+        'page_body': u'Where you can store and share bookmarks!'
+    })
+    output = template.render(variables)
+    return HttpResponse(output)
+
+def user_page(request, username):
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        raise Http404(u'Requested user not found.')
+    
+    bookmarks = user.bookmark_set.all()
+    template = get_template('user_page.html')
+    variables = Context({
+        'username': username,
+        'bookmarks': bookmarks
+    })
+    output = template.render(variables)
     return HttpResponse(output)
--- urls.py
+++ urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import *
 from bookmarks.views import *
 
-urlpatterns = patterns('', (r'^$', main_page),
+urlpatterns = patterns('', 
+    (r'^$', main_page),
+    (r'^user/(\w+)/$', user_page),
 )
